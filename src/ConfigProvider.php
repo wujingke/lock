a154cf4c6509b3f09b23cf8cec58aeddd0ddfcef
huangdijia <huangdijia@gmail.com>
2021-01-31 16:53:49 +0800

    add migrations

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -34,6 +34,12 @@ class ConfigProvider
                     'source' => __DIR__ . '/../publish/lock.php',
                     'destination' => BASE_PATH . '/config/autoload/lock.php',
                 ],
+                [
+                    'id' => 'migrations',
+                    'description' => 'migrations file of lock',
+                    'source' => __DIR__ . '/../migrations/create_lock_table.php',
+                    'destination' => BASE_PATH . '/migrations/2021_01_31_000000_create_lock_table.php',
+                ],
             ],
         ];
     }
